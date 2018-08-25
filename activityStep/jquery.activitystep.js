42238526764622afbfa9bb243e099df12eaa6b0e
BEN <714591092@qq.com>
2018-08-25 22:39:14 +0800

    update

--- activityStep/jquery.activitystep.js
+++ activityStep/jquery.activitystep.js
@@ -44,7 +44,7 @@
  			this.$element = $(element);
 
  			this._setOptions(options);
- 			this._render();
+ 			
  		},
 
  		/**
@@ -59,7 +59,7 @@
  		*/
  		_setOptions: function(options) {
 			this.options = $.extend({}, (this.options || $.fn.activityStep.defaults), options);
-
+			this._render();
  		},
 
  		/**
@@ -70,8 +70,18 @@
 
  			tempHtml = this._buildStepItem();
  			this.$element.empty();
+ 			// 传入参数判断
+ 			if(this.options.stepIndex > this.options.stepObj.length - 1) {
+ 				throw 'StepIndex Cannot be greater than stepObj`s length';
+ 				return;
+ 			}
  			if(tempHtml) {
  				this.$element.append(tempHtml);
+ 				// 用户自定义锁定任意步骤
+ 				this.$element.find('.' + this.options.itemContainerClass).find('.'+ this.options.itemContentClass).removeClass('active');
+ 				for(var i = 0; i <= this.options.stepIndex; i++) {
+ 					this.$element.find('.' + this.options.itemContainerClass).find('.'+ this.options.itemContentClass).eq(i).addClass('active');
+ 				}
  			}
  		},
 
@@ -84,16 +94,24 @@
  				itemChild = '';
 
  			if(!Array.isArray(this.options.stepObj) || this.options.stepObj == [] || this.options.stepObj.length == 0) {
- 				throw '请设置活动步骤!';
+ 				throw 'StepObj does not be empty';
  				return;
  			} else {
  				var stepObj = this.options.stepObj;
- 				for(var i=0; i<stepObj.length; i++) {
- 					itemChild = $('<span class="step-title"><span class="step-id">'+ stepObj[i].id +'</span><span class="step-name">'+ stepObj[i].name +'</span></span><div class="line"></div>');
+ 				for(var i = 0; i < stepObj.length; i++) {
+ 					// 第一步删除连线
+					 if(i === 0) {
+					 	itemChild = $('<span class="step-title"><span class="step-id">'+ stepObj[i].id +'</span><span class="step-name">'+ stepObj[i].name +'</span></span>');
+					 } else {
+					 	itemChild = $('<div class="line"></div><span class="step-title"><span class="step-id">'+ stepObj[i].id +'</span><span class="step-name">'+ stepObj[i].name +'</span></span>');
+					 }
  					itemContent = $('<div></div>');
  					itemContent.addClass(this.options.itemContentClass).append(itemChild);
+ 					// 第一步默认为active状态
+ 					if(i === 0) {
+ 						itemContent.addClass('active');
+ 					}
  					itemContainer.addClass(this.options.itemContainerClass).append(itemContent);
- 					// 最后一步删除连线
  				}
  			}
  			return itemContainer;
@@ -132,6 +150,7 @@
 
 	$.fn.activityStep.defaults = {
 		stepObj: [],
+		stepIndex: 0,
 		itemContainerClass: 'step-panel',
 		itemContentClass: 'step-item',
 	};
